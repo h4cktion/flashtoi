9666b7f896d1a7b3de1ac7fc2972f1ea70ac93e7
Wandolski Matthieu <matthieu.wandolski@gmail.com>
2025-12-18 09:11:05 +0100

    fix back bouton

--- app/backoffice/schools/[id]/page.tsx
+++ app/backoffice/schools/[id]/page.tsx
@@ -78,7 +78,7 @@ export default async function SchoolDetailsPage({
                 pendingOrders={school.pendingOrders}
               />
               <Link
-                href="/backoffice/schools"
+                href="/backoffice/dashboard"
                 className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-50"
               >
                 Retour
